aacdbbaa418ee69960e31b6dffdbb1c8a3f4f6dd
Ekram miftah <ekrammiftah236@gmail.com>
2022-03-13 13:01:45 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - entry point
  *
- *Return: 0 after printing the function
+ *return: 0 after printing the functon
  */
-int main (void)
+int main(void)
 {
-puts("\"programming is like building a multilingual puzzle"):
-return (0):
-}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+]
